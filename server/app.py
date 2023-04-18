098a3d2d46fd17716a024b45be012323f9a01b71
Jainil Patel <jainiljp72525@gmail.com>
2023-04-18 18:18:14 +0530

    initial commit

--- server/app.py
+++ server/app.py
@@ -2,7 +2,6 @@ from flask import Flask, render_template, Response, request, send_from_directory
 import requests
 from form import Form
 
-
 app = Flask(__name__, static_url_path="",static_folder="../client/build")
 
 
@@ -10,15 +9,15 @@ app = Flask(__name__, static_url_path="",static_folder="../client/build")
 def index():
     return send_from_directory(app.static_folder,"index.html")
 
+
 @app.route('/soon')
 def soon():
     return render_template('soon.html')
 
+
 @app.route("/chat")
 def chat():
     return render_template("chat.html")
-
-
 
 
 if __name__ == "__main__":
