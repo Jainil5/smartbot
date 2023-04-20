7b1e80492f94f4b99e81dfec09125f28a945bfd6
Jainil Patel <jainiljp72525@gmail.com>
2023-04-20 16:43:54 +0530

    Initial commit

--- server/app.py
+++ server/app.py
@@ -1,9 +1,16 @@
 from flask import Flask, render_template, Response, request, send_from_directory
 import requests
 from form import Form
-
+from flask_restful import *
+import openai
+openai.api_key = "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"
 app = Flask(__name__, static_url_path="",static_folder="../client/build")
+api = Api(app)
 
+def reply(x):
+    completion = openai.ChatCompletion.create(model="gpt-3.5-turbo", messages=[{"role": "user", "content": x}])
+    reply =completion["choices"][0]["message"]["content"]
+    return reply
 
 @app.route('/')
 def index():
@@ -17,7 +24,15 @@ def soon():
 
 @app.route("/chat")
 def chat():
+    print("Chat:"+"/chat")
     return render_template("chat.html")
+
+class API(Resource):
+    def get(self,data):
+        answer = reply(str(data))
+        return answer
+
+api.add_resource(API,"/ask/<string:data>")
 
 
 if __name__ == "__main__":
