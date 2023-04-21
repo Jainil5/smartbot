31b687e2f5ec0d5c5b21f0e299872692d21fd329
Jainil Patel <jainiljp72525@gmail.com>
2023-04-21 12:28:07 +0530

    initial commit

---
+++ server/api.py
@@ -0,0 +1,27 @@
+from flask import *
+from flask_restful import *
+import openai
+import requests
+
+app = Flask(__name__)
+api = Api(app)
+openai.api_key = ""
+
+
+
+def reply(x):
+    completion = openai.ChatCompletion.create(model="gpt-3.5-turbo", messages=[{"role": "user", "content": x}])
+    reply =completion["choices"][0]["message"]["content"]
+    return reply
+
+class query(Resource):
+    def get(self,ask):
+        #print(ask)
+        #print("\n",reply(ask))
+        return str(reply(ask))
+
+api.add_resource(query,"/ask/<string:ask>")
+
+
+if __name__ == "__main__":
+    app.run(debug=True,port=2000)
--- server/app.py
+++ server/app.py
@@ -1,16 +1,10 @@
-from flask import Flask, render_template, Response, request, send_from_directory
+from flask import *
 import requests
-from form import Form
 from flask_restful import *
 import openai
-openai.api_key = "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"
 app = Flask(__name__, static_url_path="",static_folder="../client/build")
 api = Api(app)
 
-def reply(x):
-    completion = openai.ChatCompletion.create(model="gpt-3.5-turbo", messages=[{"role": "user", "content": x}])
-    reply =completion["choices"][0]["message"]["content"]
-    return reply
 
 @app.route('/')
 def index():
@@ -24,17 +18,22 @@ def soon():
 
 @app.route("/chat")
 def chat():
-    print("Chat:"+"/chat")
     return render_template("chat.html")
 
-class API(Resource):
-    def get(self,data):
-        answer = reply(str(data))
-        return answer
+@app.route('/api/ask', methods=['POST'])
+def greeting():
+    data = request.get_json()
+    name = data['ask']
 
-api.add_resource(API,"/ask/<string:data>")
+    BASE = "http://127.0.0.1:2000/ask/"
+    response = requests.get(BASE + name)
+    x = response.json()
+    print(x)
+    #print(response.json())
+    #return jsonify({str(name):str(response)})
+    return jsonify({'reply': x})
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True,port=5000)
 
